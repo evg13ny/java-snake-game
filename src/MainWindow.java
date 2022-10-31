5de4e5a17aacc2ec1e64b86f0d105fbf24d31b1c
Evgeny Polyakov <evg_pol@yahoo.com>
2022-10-31 20:01:00 +0300

    added logic

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,12 +1,11 @@
 import javax.swing.*;
 
 public class MainWindow extends JFrame {
-
     public MainWindow() {
         setTitle("Snake");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(320, 345);
-        setLocation(400,400);
+        setLocation(400, 400);
         add(new GameField());
         setVisible(true);
     }
